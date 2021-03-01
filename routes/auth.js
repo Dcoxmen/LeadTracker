e425bc4fdf74157cc748a25da78022234e2585f8
Dave Cox <davedvst@gmail.com>
2021-03-01 07:24:10 -0800

    fixing broken packages

--- routes/auth.js
+++ routes/auth.js
@@ -1,23 +1,23 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
-const config = require("config");
-const auth = require("../middleware/auth");
-const { check, validationResult } = require("express-validator");
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const auth = require('../middleware/auth');
+const { check, validationResult } = require('express-validator');
 
-const User = require("../models/User");
+const User = require('../models/User');
 
 //@route   GET  api/auth
 //@desc    Get logged in user
 //@access  Private
-router.get("/", auth, async (req, res) => {
+router.get('/', auth, async (req, res) => {
   try {
-    const user = await User.findById(req.user.id).select("-password");
+    const user = await User.findById(req.user.id).select('-password');
     res.json(user);
   } catch (err) {
     console.error(err.message);
-    res.status(500).send("Server error");
+    res.status(500).send('Server error');
   }
 });
 
@@ -25,10 +25,10 @@ router.get("/", auth, async (req, res) => {
 //@desc    Auth user get token
 //@access  Public
 router.post(
-  "/",
+  '/',
   [
-    check("email", "include valid email").isEmail(),
-    check("password", "Password is required").exists()
+    check('email', 'include valid email').isEmail(),
+    check('password', 'Password is required').exists()
   ],
   async (req, res) => {
     const errors = validationResult(req);
@@ -41,12 +41,12 @@ router.post(
       let user = await User.findOne({ email });
 
       if (!user) {
-        return res.status(400).json({ msg: "Invalid Credentials" });
+        return res.status(400).json({ msg: 'Invalid Credentials' });
       }
       const isMatch = await bcrypt.compare(password, user.password);
 
       if (!isMatch) {
-        return res.status(400).json({ msg: "Invalid Credentials" });
+        return res.status(400).json({ msg: 'Invalid Credentials' });
       }
 
       const payload = {
@@ -57,7 +57,7 @@ router.post(
 
       jwt.sign(
         payload,
-        config.get("jwtSecret"),
+        config.get('jwtSecret'),
         {
           expiresIn: 360000
         },
@@ -68,7 +68,7 @@ router.post(
       );
     } catch (err) {
       console.error(err.message);
-      res.status(500).send("Server Error");
+      res.status(500).send('Server Error');
     }
   }
 );
